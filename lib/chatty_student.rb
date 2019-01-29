7684cd53d5e0aad6162cf92a247b145ecf443a2e
Stephen Wright <scw248@gmail.com>
2019-01-29 21:53:06 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,4 +1,7 @@
 class ChattyStudent < Student
   
-  
+  def hello
+    super
+    puts "Hey there! I'm so excited to learn stuff.\Howare you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...
+  end
 end
